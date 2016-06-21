63881acbd23d5e6f078e7cdf5350f8f1235a4dcd
altitudebreath <altitudebreath@gmail.com>
2016-06-21 11:37:07 +0300

    Configurator: map type is added, trim spaces functionality regulated

--- lib-src/10.2.tool.configurator.js
+++ lib-src/10.2.tool.configurator.js
@@ -8,6 +8,9 @@
 function Configurator(ssPoolOrData, options, isClientSide){
     var t = this;
     t._opt = options;
+    if (typeof t._opt.doTrim === UNDEF){
+        t._opt.doTrim = true;
+    }
     if (isClientSide){
         t._isClient = true;
         t._data = ssPoolOrData;
@@ -15,26 +18,59 @@ function Configurator(ssPoolOrData, options, isClientSide){
         t._ssPool = ssPoolOrData;
     }
     t._params = null;
-
-    t._handlers = {
-        'string': function(val){return val.toString();},
-        'integer': function(val){return parseInt(val);},
-        'float': function(val){return parseFloat(val);},
-        'JSON': function(val){return JSON.parse(val.toString());},
-        'date': function(val){return new Date(val);},
-        'list': function(val, splitter){
-            return val.toString()
-                .split(Lib.util.makeRegex('\\s*', splitter, '\\s*'));
-        },
-        'array': function(val, terminator, row){
-            return row.slice(0, row.indexOf(terminator || ""));
-        }
-    }
-    
 }
 
-Configurator.prototype._getByType = function (row, value, atype, splitterOrTerminator) {
-    return this._handlers[atype](value, splitterOrTerminator, row);
+Configurator.prototype._handlers = {
+    'string': function(val){return String(val);},
+    'string.trim': function(val){return String(val).trim();},
+    'integer': function(val){return parseInt(val);},
+    'float': function(val){return parseFloat(val);},
+    'JSON': function(val){return JSON.parse(val.toString());},
+    'date': function(val){return new Date(val);},
+    'list': function(val, splitter){
+        return val.toString().split(splitter || ',');
+    },
+    'list.trim': function(val, splitter){
+        return String(val).trim()
+            .split(Lib.util.makeRegex('\\s*', (splitter || ','), '\\s*'));
+    },
+    'array': function(val, terminator, row){
+        return row.slice(0, row.indexOf(terminator || ""));
+    },
+    'array.trim': function(val, terminator, row){
+        return row.slice(0, row.indexOf(terminator || ""))
+            .map(function(el){return el.trim();});
+    },
+    'map': function(val, terminator, row, nextRow){
+        var row1 = row.slice(0, row.indexOf(terminator || ""));
+        var row2 = nextRow.slice(0, row.indexOf(terminator || ""));
+
+        var res = {
+            D: {},  //direct mapping
+            R: {}    //reverse mapping
+        };
+        
+        var i;
+        if (this._opt.doTrim) {
+            for (i = 0; i < row1.length; i++) {
+                res.D[String(row1[i])] = String(row2[i]);
+                res.R[String(row2[i])] = String(row1[i]);
+            }
+        }else{
+            for (i = 0; i < row1.length; i++) {
+                res.D[String(row1[i]).trim()] = String(row2[i]).trim();
+                res.R[String(row2[i]).trim()] = String(row1[i]).trim();
+            }
+        }
+        return res;
+    }
+};
+
+
+Configurator.prototype._getByType = function (row, value, atype, splitterOrTerminator, nextRow) {
+    return (this._handlers[atype + (this._opt.doTrim ? '.trim' : '')] 
+        || this._handlers[atype]) //in case .trim is not available
+        .call(this, value, splitterOrTerminator, row, nextRow);
 };
 
 Configurator.prototype.get = function () {
@@ -49,23 +85,34 @@ Configurator.prototype.get = function () {
             res = t._ssPool.get(t._opt.ADMIN_SPREADSHEET_ID, t._opt.PARAMETERS_SHEET_NAME);
             values = res.sheet.getDataRange().getValues();
         }
-        var params = {};
+        t._params = {};
+        var nextRowSlice = null;
         for (var i = t._opt.PARAM_ROWS_STARTS_FROM - 1; i < values.length; i++){
             var row = values[i];
             var name = row[t._opt.PARAM_NAME_POSITION - 1];
             if (name && ! /^\s*\/\//.test(name)) {   // '//' to comment'
-                Lib.util.walkNamespace(params, name,
+                var atype = row[t._opt.PARAM_TYPE_POSITION - 1];
+                if (atype === 'map'){
+                    i++;
+                    if (i < values.length){
+                        nextRowSlice = values[i].slice(t._opt.PARAM_VALUE_POSITION - 1)
+                    }else{
+                        throw Error("Map '" + name + "': there are no second row")
+                    }
+                }else{
+                    nextRowSlice = null;
+                }
+                Lib.util.walkNamespace(t._params, name,
                     t._getByType(
                         row.slice(t._opt.PARAM_VALUE_POSITION - 1),
                         row[t._opt.PARAM_VALUE_POSITION - 1],
-                        row[t._opt.PARAM_TYPE_POSITION - 1],
-                        row[t._opt.ITEMS_SPLITTER_POSITION - 1]
+                        atype,
+                        row[t._opt.ITEMS_SPLITTER_POSITION - 1],
+                        nextRowSlice
                     )
                 );
             }
         }
-        t._params = params;
-        
         return t._params;
     }
 };
