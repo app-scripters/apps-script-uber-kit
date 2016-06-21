a33f4a8e9a399bb788690cea1ac7883220f1741b
altitudebreath <altitudebreath@gmail.com>
2016-06-21 11:34:23 +0300

    simple template improvement: works now for multiple contexts with or without override

--- lib-src/1.3.util.simple-templates.js
+++ lib-src/1.3.util.simple-templates.js
@@ -1,7 +1,37 @@
 //very simple templating
-Lib.util.compile = function(s, args_dict){
-    return s.replace(/{{\s*\w+\s*}}/ig, function(capture){
-        return args_dict[capture.match(/\w+/i)]
-    })
+/**
+ * 
+ * @param s - template string with {{ var }} variable inclusions
+ * @param argsDict - context object or array ob context objects
+ * @param overrideDicts - for array of contexts case: process all contexts and override, or stop on first matching context var
+ * @returns {void|XML|string|*}
+ */
+Lib.util.bindTemplate = function(s, argsDict, overrideDicts){
+    var capturer = Lib.util.isArray(argsDict) ? 
+        Lib.util.bindTemplate._arrayCtxCapture(argsDict, overrideDicts) : 
+        Lib.util.bindTemplate._simpleCapture(argsDict);
+    
+    return s.replace(/{{\s*\w+\s*}}/ig, capturer);
 };
 
+Lib.util.bindTemplate._simpleCapture = function(argsDict){
+    return function(capture){
+        return argsDict[capture.match(/\w+/i)]
+    }  
+};
+
+
+Lib.util.bindTemplate._arrayCtxCapture = function(arrayOfArgsDict, overrideDicts){
+    return function(capture){
+        var result = '';
+        var key = capture.match(/\w+/i); 
+        for (var i = 0; i < arrayOfArgsDict.length; i++) {
+            var dict = arrayOfArgsDict[i];
+            if (key in dict){
+                result = dict[key];
+                if (! overrideDicts) break;
+            }
+        }
+        return result;
+    }  
+};
